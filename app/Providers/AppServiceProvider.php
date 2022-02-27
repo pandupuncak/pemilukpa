2846b358abe3fbfa7143f37593763380cf3ff9b0
pandupuncak <pandupuncak@gmail.com>
2022-02-27 16:10:47 +0700

    redoing previous change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
         Inertia::share([
             'errors' => function () {
                 return Session::get('errors')
